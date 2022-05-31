be3ae11dfe504592c118684b5e4dce9c4ef7cd04
oguzky7 <oguzky7@gmail.com>
2022-05-31 19:55:19 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,6 +4,7 @@ public class gitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("today we learn github");
+		System.out.println("asdasfasf");
 		
 	}
 }
