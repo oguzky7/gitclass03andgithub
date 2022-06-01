c43fdedcf1ab1d3a47abc127c3f53df9557e3f06
oguzky7 <oguzky7@gmail.com>
2022-05-31 21:07:24 -0400

    3rd commit

---
+++ src/github/example1.java
@@ -0,0 +1,10 @@
+package github;
+
+public class example1 {
+	public static void main(String[] args) {
+
+
+		System.out.println("I am a code");
+	
+	}
+}
--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,7 +4,7 @@ public class gitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("today we learn github");
-		System.out.println("asdasfasf");
+		System.out.println("today ");
 		
 	}
 }
